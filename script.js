80a23b1d5b7cb1b109d9da8ef8721b0fe431f71e
DavidSchuchert <Davidschuchert@icloud.com>
2024-07-19 19:02:52 +0200

    added changeable playernames

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let life = [20, 20];
-window.addEventListener("load", start)
+let playerNames = ["", ""];
+
+window.addEventListener("load", start);
 
 function start() {
+    playerNames[0] = localStorage.getItem("PlayerOneName") || "";
+    playerNames[1] = localStorage.getItem("PlayerTwoName") || "";
     generateHtml();
 }
 
@@ -11,18 +15,28 @@ function iniEvLi() {
     let buttonPlusTwo = document.querySelector('#p2p');
     let buttonMinusTwo = document.querySelector('#p2m');
     let buttonReset = document.querySelector('#reset');
+    let iPlayerOne = document.querySelector('#iPlayerOne');
+    let iPlayerTwo = document.querySelector('#iPlayerTwo');
 
     buttonPlusOne.addEventListener('click', () => addLife(0));
     buttonMinusOne.addEventListener('click', () => minusLife(0));
     buttonPlusTwo.addEventListener('click', () => addLife(1));
     buttonMinusTwo.addEventListener('click', () => minusLife(1));
     buttonReset.addEventListener('click', resetLife);
+    iPlayerOne.addEventListener('input', () => changePlayerName(0, iPlayerOne.value));
+    iPlayerTwo.addEventListener('input', () => changePlayerName(1, iPlayerTwo.value));
+}
+
+function changePlayerName(index, value) {
+    playerNames[index] = value;
+    localStorage.setItem(index === 0 ? "PlayerOneName" : "PlayerTwoName", value);
 }
 
 function resetLife() {
     life = [20, 20];
     generateHtml();
 }
+
 function addLife(i) {
     life[i] += 1;
     generateHtml();
@@ -38,7 +52,7 @@ function generateHtml() {
         `
             <h1>Multi Life Counter</h1>
         <div class="player reverse">
-            <h2>Player</h2>
+            <input type="text" id="iPlayerOne" value="${playerNames[0]}" placeholder="Player One Name">
             <div class="life">
                 <button id="p1m" class="minus">-</button>
                 <p>${life[0]}</p>
@@ -47,15 +61,14 @@ function generateHtml() {
         </div>
         <button id="reset">Reset</button>
         <div class="player">
-            <h2>Player</h2>
+            <input type="text" id="iPlayerTwo" value="${playerNames[1]}" placeholder="Player Two Name">
             <div class="life">
                 <button id="p2m" class="minus">-</button>
                 <p>${life[1]}</p>
                 <button id="p2p" class="plus">+</button>
             </div>
         </div>
-        `
+        `;
 
     iniEvLi(); // Event-Listener nach der DOM-Aktualisierung erneut hinzufügen
 }
-
